fb88fc2c4aea09533b9079c2c6015dceed1bca9c
quantingan <15707441210@163.com>
2020-03-17 17:27:06 +0800

    3yue17月销售表

--- src/main/java/com/jiebao/platfrom/crm/controller/CrmContractOrderController.java
+++ src/main/java/com/jiebao/platfrom/crm/controller/CrmContractOrderController.java
@@ -1,15 +1,53 @@
 package com.jiebao.platfrom.crm.controller;
 
 
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.jiebao.platfrom.common.domain.QueryRequest;
+import com.jiebao.platfrom.common.exception.JiebaoException;
+import com.jiebao.platfrom.crm.domain.CrmOrderMonth;
+import com.jiebao.platfrom.crm.service.ICrmContractOrderService;
+import com.jiebao.platfrom.system.domain.User;
+import com.wuwenze.poi.ExcelKit;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+
 /**
  * @author Sinliz
  */
+@Slf4j
 @RestController
 @RequestMapping("/crm/contract-order")
+@DS("crm")
 public class CrmContractOrderController {
+    @Autowired
+    ICrmContractOrderService orderService;
 
+    //月销售记录整理
+    @GetMapping("List")
+    public List<CrmOrderMonth> crmContractOrderList(String month, String year) {
+        return orderService.crmContractOrderList(year, month);
+    }
+
+    @PostMapping("excel")
+
+    public void export(String year, String month, HttpServletResponse response) throws JiebaoException {
+        try {
+            List<CrmOrderMonth> crmOrderMonths = orderService.crmContractOrderList(year, month);
+            ExcelKit.$Export(CrmOrderMonth.class, response).downXlsx(crmOrderMonths, false);
+        } catch (Exception e) {
+            String message = "导出Excel失败";
+            log.error(message, e);
+            throw new JiebaoException(message);
+        }
+    }
 }
--- src/main/java/com/jiebao/platfrom/crm/dao/CrmContractOrderMapper.java
+++ src/main/java/com/jiebao/platfrom/crm/dao/CrmContractOrderMapper.java
@@ -1,11 +1,27 @@
 package com.jiebao.platfrom.crm.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
 import com.jiebao.platfrom.crm.domain.CrmContractOrder;
+import com.jiebao.platfrom.crm.domain.CrmOrderMonth;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.sql.DataSourceDefinitions;
+import java.util.List;
 
 /**
  * @author Sinliz
  */
+@Repository
 public interface CrmContractOrderMapper extends BaseMapper<CrmContractOrder> {
-
+    @Select("<script>select ht_id as id,ht_customerid as htName,IFNULL(ht_summoney,0) as htSumMoney,ht_date " +
+            "as htDate,ht_preside as htPerson,(select IFNULL(sum(bill_money),0) from crm_bill_record where bill_parentid=c.ht_id) " +
+            "as htKpMoney,(select IFNULL(avg(bill_shuilv),0) from crm_bill_record  where bill_parentid=c.ht_id) as htSjMoney," +
+            " c.ht_summoney-(select IFNULL(sum(hk_money),0) from crm_cashback_record where hk_htid=c.ht_id ) as htYqMoney " +
+            "from crm_contract_order c    where c.ht_date like '${year}/${month}%' ORDER BY c.ht_date DESC  </script>")
+    List<CrmOrderMonth> crmContractOrderList( String year, String month);  //
 }
+// and c.ht_date like '#{year}'+'/#{month}%'
