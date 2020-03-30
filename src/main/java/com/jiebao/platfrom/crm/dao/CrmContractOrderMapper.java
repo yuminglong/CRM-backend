fe45d5c97276bb060fced2100abeb13f2920a83e
quantingan <19918830011@163.com>
2020-03-30 13:52:05 +0800

    3.3.

--- src/main/java/com/jiebao/platfrom/crm/dao/CrmContractOrderMapper.java
+++ src/main/java/com/jiebao/platfrom/crm/dao/CrmContractOrderMapper.java
@@ -17,11 +17,19 @@ import java.util.List;
  */
 @Repository
 public interface CrmContractOrderMapper extends BaseMapper<CrmContractOrder> {
-    @Select("<script>select ht_id as id,ht_customerid as htName,IFNULL(ht_summoney,0) as htSumMoney,ht_date " +
-            "as htDate,ht_preside as htPerson,(select IFNULL(sum(bill_money),0) from crm_bill_record where bill_parentid=c.ht_id) " +
-            "as htKpMoney,(select IFNULL(avg(bill_shuilv),0) from crm_bill_record  where bill_parentid=c.ht_id) as htSjMoney," +
-            " c.ht_summoney-(select IFNULL(sum(hk_money),0) from crm_cashback_record where hk_htid=c.ht_id ) as htYqMoney " +
+    @Select("<script>select ht_id as id," +  //id
+            "ht_customerid as htName," +//name
+            "IFNULL(ht_summoney,0) as htSumMoney," +  //总金额
+            "ht_date " + "as htDate," +  //合同时间
+            "ht_preside as htPerson," + //相关负责人
+            "(select IFNULL(sum(bill_money),0) from crm_bill_record where bill_parentid=c.ht_id) " +
+            "as htKpMoney," +  //开票
+            "(select IFNULL(avg(bill_shuilv),0) from crm_bill_record  where bill_parentid=c.ht_id)*0.01*IFNULL(ht_summoney,0) as htSjMoney," + //税金
+            " (select 100-IFNULL(avg(bill_shuilv),0) from crm_bill_record  where bill_parentid=c.ht_id)*0.01*IFNULL(ht_summoney,0) as htFyMoney," + //费用总计
+            "IFNULL(ht_summoney,0) as htYsMoney," +  //应收账款
+            " c.ht_summoney-(select IFNULL(sum(hk_money),0) from crm_cashback_record where hk_htid=c.ht_id ) as htYqMoney " +//逾期
+
             "from crm_contract_order c    where c.ht_date like '${year}/${month}%' ORDER BY c.ht_date DESC  </script>")
-    List<CrmOrderMonth> crmContractOrderList( String year, String month);  //
+    List<CrmOrderMonth> crmContractOrderList(String year, String month);  //
 }
 // and c.ht_date like '#{year}'+'/#{month}%'
